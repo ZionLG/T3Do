53396dad8e13f6f5204ffc89f182d7edb9e933e2
לירן <liran12369@gmail.com>
2023-07-30 20:35:12 +0300

    remvoe example code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { todoRouter } from "./routers/todo";
 
@@ -8,7 +7,6 @@ import { todoRouter } from "./routers/todo";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   todo: todoRouter,
 });
 
